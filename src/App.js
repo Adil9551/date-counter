39d037a3305b26f54073d0aa515437f4bba6590f
Adil9551 <15adil95@gmail.com>
2023-08-07 23:37:48 -0700

    react step and counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app--body">
+      <div className="app--main">
+        <Step />
+      </div>
     </div>
   );
+
+  function Step() {
+    const [step, setStep] = useState(5);
+    const [count, setCount] = useState(5);
+
+    const date = new Date();
+
+    function handleStep(e) {
+      if (step > 1 && e.target.dataset.operation === "subtract")
+        setStep((s) => s - 1);
+
+      if (e.target.dataset.operation === "add") setStep((s) => s + 1);
+    }
+
+    function handleCount(e) {
+      if (count - step > 0 && e.target.dataset.operation === "subtract")
+        setCount((s) => s - step);
+
+      if (e.target.dataset.operation === "add") setCount((s) => s + step);
+    }
+
+    return (
+      <>
+        <div className="app--container">
+          <button
+            onClick={handleStep}
+            className="btn"
+            data-operation="subtract"
+          >
+            -
+          </button>
+          <span>Step: {step}</span>
+          <button onClick={handleStep} className="btn" data-operation="add">
+            +
+          </button>
+        </div>
+        <div className="app--container">
+          <button
+            onClick={handleCount}
+            className="btn"
+            data-operation="subtract"
+          >
+            -
+          </button>
+          <span>Count: {count}</span>
+          <button onClick={handleCount} className="btn" data-operation="add">
+            +
+          </button>
+        </div>
+        <div>
+          {count} days from today is{" "}
+          {new Date(date.setDate(date.getDate() + count)).toDateString()}
+        </div>
+      </>
+    );
+  }
+
+  // function Count() {
+  //   const [count, setCount] = useState(5);
+
+  //   function handleCount(e) {
+  //     console.log(e.target.dataset.operation);
+  //     if (count > 0 && e.target.dataset.operation === "subtract")
+  //       setCount((s) => s - 1);
+
+  //     if (e.target.dataset.operation === "add") setCount((s) => s + 1);
+  //   }
+
+  //   return (
+  //     <div className="app--container">
+  //       <button onClick={handleCount} className="btn" data-operation="subtract">
+  //         -
+  //       </button>
+  //       <span>Count: {count}</span>
+  //       <button onClick={handleCount} className="btn" data-operation="add">
+  //         +
+  //       </button>
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
